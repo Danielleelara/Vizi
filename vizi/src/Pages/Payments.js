7b0e9dcae701ea176664e1651db6bdf995ff8952
Danielle Souza <dany.sa1@hotmail.com>
2022-04-22 19:39:31 -0300

    feat: criando página de pagamentos

--- vizi/src/Pages/Payments.js
+++ vizi/src/Pages/Payments.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import api from '../api';
 
 export default function Payments() {
+  const [payments, setPayments] = useState([]);
+
+  useEffect(()=>{
+      async function getProducts() {
+          const response = await api.get(`payments/`);
+          setPayments(response.data);
+        }
+        getProducts();
+  })
+
+  console.log('pagamentos', payments)
   return (
-    <div>payments</div>
+    <div>
+      <table>
+      {
+        payments.map((payment)=> (
+          <tr>
+            <td>Data de Pagamento: {payment.paymentDate}`</td>
+            <td>Valor da Compra: R${payment.amount}</td>
+            <td>Status do Pagamento: {payment.status}</td>
+          </tr>
+        ))
+      }
+      </table>
+    </div>
   )
 }
+

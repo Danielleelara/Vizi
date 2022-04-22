6bcf98c2af059965495dcafb474cd231d4194e41
Danielle Souza <dany.sa1@hotmail.com>
2022-04-22 19:22:56 -0300

    feat: criando home e api

--- vizi/src/Pages/Home.js
+++ vizi/src/Pages/Home.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import api from '../api';
+import styles from './Home.module.css';
 
-export default function Hsome() {
+export default function Home() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(()=>{
+        async function getProducts() {
+            const response = await api.get(`products/`);
+            setProducts(response.data);
+          }
+          getProducts();
+    })
+
   return (
-    <div>home</div>
+    
+        <div className={styles.container}>
+       { products.map ((product)=> {
+            return (                
+                    <div className={styles.card}>
+                        <img src={product.thumb} alt="Imagem do produto"/>
+                        <div className="card-body">
+                            <h5 className="card-title">{product.name}</h5>
+                            <p className="card-text">R$ {product.price}</p>
+                        
+                        </div>
+                    </div>
+            )
+        })
+    }
+
+
+    </div>
   )
 }

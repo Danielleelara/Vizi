af14c2ee2acd400184c10da8fa04f48c12070233
Danielle Souza <dany.sa1@hotmail.com>
2022-04-22 21:05:47 -0300

    feat: iniciando tela de detalhes

--- vizi/src/Pages/Details.js
+++ vizi/src/Pages/Details.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { useEffect, useState } from "react";
+import api from '../api';
+import { useParams } from "react-router";
 
 export default function Details() {
+  const { id } = useParams();
+  const [details, setDetails] = useState([]);
+ 
+
+  useEffect(() => {
+    async function getDetails(id) {
+      const response = await api.get(`payments/${id}`);
+      setDetails(response.data);
+     
+    }
+      getDetails(id);
+  }, [id]);
+
+  console.log('details', details);
+
   return (
-    <div>details</div>
+    <div>
+      {
+        details.map((detail)=> (
+         
+            <div>
+            <p>aqui</p>
+            </div>
+        
+        ))
+      }
+    </div>
   )
 }
+

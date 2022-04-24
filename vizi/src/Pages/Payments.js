c691d62e95e017561cf481d3ec2323c619cb3c93
Danielle Souza <dany.sa1@hotmail.com>
2022-04-24 14:20:18 -0300

    feat: eslizando payments

--- vizi/src/Pages/Payments.js
+++ vizi/src/Pages/Payments.js
@@ -1,38 +1,56 @@
-import React, { useEffect, useState } from 'react';
-import api from '../api';
+import React, { useEffect, useState } from "react";
+import api from "../api";
 import { Link } from "react-router-dom";
-import { CgEye} from "react-icons/cg";
+import { CgEye } from "react-icons/cg";
+import styles from "./Payments.module.css";
 
 export default function Payments() {
   const [payments, setPayments] = useState([]);
 
-  useEffect(()=>{
-      async function getProducts() {
-          const response = await api.get(`payments/`);
-          setPayments(response.data);
-        }
-        getProducts();
-  })
+  useEffect(() => {
+    async function getProducts() {
+      const response = await api.get(`payments/`);
+      setPayments(response.data);
+    }
+    getProducts();
+  });
+
+  const formatDate = (dateInString) => {
+    return new Intl.DateTimeFormat("pt-BR", {
+      timeZone: "UTC",
+    }).format(new Date(dateInString).getTime());
+  };
 
   return (
-    <div>
-      <table className="list-group">
-      {
-        payments.map((payment)=> (
-          <td>
-            <tr className="list-group-item">Data de Pagamento: {payment.paymentDate}</tr>
-            <tr className="list-group-item">Valor da Compra: R${payment.amount}</tr>
-            <tr className="list-group-item">Status do Pagamento: {payment.status}</tr>
-            <span>
-            <Link to={`/details/${payment.id}`}>
-                    <CgEye  size={20} color="gray" />
-            </Link>
-            </span>
-          </td>
-        ))
-      }
+    <table className="table table-striped table-light mt-3">
+      <thead>
+        <tr>
+          <th>Data de Pagamento:</th>
+          <th>Valor da Compra:</th>
+          <th>Status do Pagamento:</th>
+          <th>Ações:</th>
+        </tr>
+      </thead>
+      {payments.map((payment) => (
+        <tbody>
+          <tr>
+            <td>{formatDate(payment.paymentDate)}</td>
+            <td>R$ {payment.amount}</td>
+            <td>
+              {payment.status === "PAID"
+                ? "Pagamento Realizado"
+                : "Pagamento Pendente"}
+            </td>
+            <td>
+              <span className={styles.container}>
+                <Link to={`/details/${payment.id}`}>
+                  <CgEye size={20} color="black" />
+                </Link>
+              </span>
+            </td>
+          </tr>
+        </tbody>
+      ))}
     </table>
-    </div>
-  )
+  );
 }
-
